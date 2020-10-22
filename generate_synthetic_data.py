e75a7a464e84d5484dfdff69f2c441ddbad64807
Adam Green <adam.green@colorado.edu>
2020-10-22 12:59:23 -0600

    initial commit

---
+++ generate_synthetic_data.py
@@ -0,0 +1,26 @@
+import defectData
+import data_utils
+
+
+# Generate n images using the masks defined in the maskTemplate.yaml file
+#This will generate a random number of defects, then thermally evolve the sequence
+#to give brownian flucuations. Then the augmentation masks will be applied.
+res = 64
+n = 2000
+#defectData.thermal_noise_sequence(n,res)
+
+#Split the total data into a training and test split. Keep these physically seperate
+#so that we can run fair comparisons across all of our models.
+#data_utils.train_test_split('../../data', '../../data')
+
+#data_utils.create_gauss_mask('../train_set{}_sparse/'.format(res), n = 3)
+#data_utils.create_gauss_mask('../test_set{}_sparse/'.format(res), n = 3)
+data_utils.create_gauss_mask('../train_set{}_sparse/'.format(res), sig = 3)
+data_utils.create_gauss_mask('../test_set{}_sparse/'.format(res), sig = 3)
+#data_utils.create_unet_mask('../proto_test_set/'.format(res), n = 3)
+
+#data_utils.proto_train_test_split('../../data/train_set', '../../data')
+
+
+
+
