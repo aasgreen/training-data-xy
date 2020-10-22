d76697a818609d1066cc0edbc3ec2e82431983c5
Adam Green <adam.green@colorado.edu>
2020-10-22 13:04:19 -0600

    made generate_synth data ready to run

--- generate_synthetic_data.py
+++ generate_synthetic_data.py
@@ -7,11 +7,14 @@ import data_utils
 #to give brownian flucuations. Then the augmentation masks will be applied.
 res = 64
 n = 2000
-#defectData.thermal_noise_sequence(n,res)
+defectData.thermal_noise_sequence(n,res)
 
 #Split the total data into a training and test split. Keep these physically seperate
 #so that we can run fair comparisons across all of our models.
-#data_utils.train_test_split('../../data', '../../data')
+data_utils.train_test_split('../../data', '../../data')
+
+
+#now, take the ground truth files, and create a mask on top of them (either a block mask (unet) or a gaussian kernal mask gauss)
 
 #data_utils.create_gauss_mask('../train_set{}_sparse/'.format(res), n = 3)
 #data_utils.create_gauss_mask('../test_set{}_sparse/'.format(res), n = 3)
